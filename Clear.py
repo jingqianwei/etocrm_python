cab3f92bbb4310351bf3e776ca02c20355d355c0
chinwe.jing <1207511201@qq.com>
2018-11-13 17:47:15 +0800

    python练习

--- Clear.py
+++ Clear.py
@@ -7,7 +7,6 @@
 import os
 import shutil
 
-
 # 创建文件夹
 def create_folder(path_sub_folder):
     if not os.path.exists(path_sub_folder):
@@ -30,10 +29,27 @@ def is_folder_file(path):
             return True
     return False
 
+# 保存说明文档
+def save_as_txt(path, deal_file, info_list):
+    file_name = os.path.join(path, deal_file)
+    with open(file_name, 'w', encoding='utf-8') as f:
+        for val in info_list:
+            f.write(val + '\n')
+
+# 读取说明文档
+def read_as_txt(path, deal_file):
+    file_name = os.path.join(path, deal_file)
+    with open(file_name, 'r', encoding='utf-8') as f:
+        contents = f.read().split('\n')  # 字符串转化为数组
+        contents.pop()  # 删除最后一个空元素
+        return contents
+
 class Clear(object):
-    def __init__(self, path):
+    def __init__(self, path, deal_file):
         # 这里是你要整理的文件夹，可以根据需要赋予相应的路径
         self.path_root = os.path.join(os.getcwd(), path)
+        self.deal_file = deal_file
+        self.new_create_folder = []
 
     def collect(self):
         """
@@ -50,28 +66,45 @@ class Clear(object):
 
                 create_folder(path_sub_folder)
                 move_file(file_old, file_new)
-
+                # 记录哪些文件夹是新创建的
+                if path_sub_folder not in self.new_create_folder:
+                    self.new_create_folder.append(path_sub_folder)
+        # 写入说明文件中
+        save_as_txt(self.path_root, self.deal_file, self.new_create_folder)
 
     def extract(self):
         """
         回退文件上面整理操作
         """
-        for root, dirs, files in os.walk(self.path_root):
-            if dirs:
-                for d in dirs:
-                    path_d = os.path.join(self.path_root, d)
-                    for f in os.listdir(path_d):
-                        file_old = os.path.join(path_d, f)
-                        file_new = os.path.join(self.path_root, f)
+        # # 粗暴型回退，不满足回退到跟整理之前的一样
+        # for root, dirs, files in os.walk(self.path_root):
+        #     if dirs:
+        #         for d in dirs:
+        #             path_d = os.path.join(self.path_root, d)
+        #             for f in os.listdir(path_d):
+        #                 file_old = os.path.join(path_d, f)
+        #                 file_new = os.path.join(self.path_root, f)
+        #
+        #                 move_file(file_old, file_new)
+        #             delete_folder(path_d)
+        # os.remove(os.path.join(self.path_root, self.deal_file))  # 删除整理文件
 
-                        move_file(file_old, file_new)
-                    delete_folder(path_d)
+        # 优雅型回退，满足回退到跟整理之前的一样
+        if len(read_as_txt(self.path_root, self.deal_file)) > 0:  # 存在新创建的文件夹
+            for path_d in read_as_txt(self.path_root, self.deal_file):
+                for f in os.listdir(path_d):
+                    file_old = os.path.join(path_d, f)
+                    file_new = os.path.join(self.path_root, f)
+
+                    move_file(file_old, file_new)
+                delete_folder(path_d)
+            os.remove(os.path.join(self.path_root, self.deal_file))  # 删除整理文件
 
     def main(self):
         """
         统一入口
         """
-        if is_folder_file(self.path_root):  # 存在文件，整理
+        if not os.path.isfile(os.path.join(self.path_root, self.deal_file)):  # 不存在整理文件
             self.collect()
             print('整理成功')
         else:  # 不存在就后退
@@ -79,5 +112,5 @@ class Clear(object):
             print('回退成功')
 
 if __name__ == '__main__':
-    clear = Clear('\\Users\chinwe.jing\Desktop\资料')
+    clear = Clear('\\Users\\chinwe.jing\\Desktop\\资料', '整理说明文档.txt')
     clear.main()
